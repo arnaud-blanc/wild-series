e54a3a5024cb3881806a9af8e746f79c7e8001ac
arnaud-blanc <mail.github2@blaca.eu>
2021-05-29 07:13:10 +0200

    05 - Symfony : Le routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -12,11 +12,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+
+/**
+ * @Route("/programs/", name="program_")
+ */
 class ProgramController extends AbstractController
 
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("", name="index")
      */
     public function index(): Response
 
@@ -28,6 +32,14 @@ class ProgramController extends AbstractController
      
          ]);
 
+    }
+
+    /**
+     * @Route("{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 
 }
